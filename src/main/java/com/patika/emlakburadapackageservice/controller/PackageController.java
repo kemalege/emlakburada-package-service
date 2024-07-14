97643b5208e2a2dc26da787b967b3df73ca63da6
kemalege <cirpanliege@gmail.com>
2024-07-14 13:29:24 +0300

    Create controller buyPackage

---
+++ src/main/java/com/patika/emlakburadapackageservice/controller/PackageController.java
@@ -0,0 +1,23 @@
+package com.patika.emlakburadapackageservice.controller;
+
+import com.patika.emlakburadapackageservice.dto.request.PurchasePackageRequest;
+import com.patika.emlakburadapackageservice.service.AdPackageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/packages")
+public class PackageController {
+
+    private final AdPackageService packageService;
+
+    @PostMapping
+    public ResponseEntity<Void> buyPackage(@RequestBody PurchasePackageRequest request){
+        packageService.redirectToPaymentService(request);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+}
