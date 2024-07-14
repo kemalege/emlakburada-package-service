1d8938660f8402d28a3e9881c74fc8c1ec2ed11f
kemalege <cirpanliege@gmail.com>
2024-07-14 13:06:33 +0300

    Create exceptions

---
+++ src/main/java/com/patika/emlakburadapackageservice/exception/EmlakBuradaException.java
@@ -0,0 +1,8 @@
+package com.patika.emlakburadapackageservice.exception;
+
+public class EmlakBuradaException extends RuntimeException {
+    public EmlakBuradaException(String message) {
+        super(message);
+    }
+}
+
---
+++ src/main/java/com/patika/emlakburadapackageservice/exception/ExceptionMessages.java
@@ -0,0 +1,9 @@
+package com.patika.emlakburadapackageservice.exception;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ExceptionMessages {
+    public static final String PACKET_NOT_FOUND = "Paket bulunamadı.";
+}
