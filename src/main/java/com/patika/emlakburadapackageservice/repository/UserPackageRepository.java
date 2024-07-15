af57471e57e21dabb8d83fc168ab6ddaa52c7abe
kemalege <cirpanliege@gmail.com>
2024-07-15 03:23:15 +0300

    Create findAllByUserId

--- src/main/java/com/patika/emlakburadapackageservice/repository/UserPackageRepository.java
+++ src/main/java/com/patika/emlakburadapackageservice/repository/UserPackageRepository.java
@@ -3,5 +3,8 @@ package com.patika.emlakburadapackageservice.repository;
 import com.patika.emlakburadapackageservice.model.UserPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserPackageRepository extends JpaRepository<UserPackage, Long> {
+    List<UserPackage> findAllByUserId(Long userId);
 }
