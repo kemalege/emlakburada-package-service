d80ddd972b6ad42dc05311b70ece83c3cc455a24
kemalege <cirpanliege@gmail.com>
2024-07-15 03:21:15 +0300

    Add new exception messages

--- src/main/java/com/patika/emlakburadapackageservice/exception/ExceptionMessages.java
+++ src/main/java/com/patika/emlakburadapackageservice/exception/ExceptionMessages.java
@@ -6,4 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExceptionMessages {
     public static final String PACKET_NOT_FOUND = "Paket bulunamadı.";
+    public static final String INSUFFICIENT_AD_COUNT = "Paketin geçerlilik süresi dolmuştur.";
+    public static final String PACKET_EXPIRED = "Yayınlama hakkınız dolmuştur.";
 }
