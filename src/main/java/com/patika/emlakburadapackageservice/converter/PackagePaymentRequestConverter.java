e9228da5c0ddd6142136117be34f5a134bc05f2a
kemalege <cirpanliege@gmail.com>
2024-07-22 04:16:07 +0300

    Fix import

--- src/main/java/com/patika/emlakburadapackageservice/converter/PackagePaymentRequestConverter.java
+++ src/main/java/com/patika/emlakburadapackageservice/converter/PackagePaymentRequestConverter.java
@@ -1,13 +1,14 @@
 package com.patika.emlakburadapackageservice.converter;
 
+import com.patika.emlakburadapackageservice.dto.request.PackagePaymentRequest;
 import com.patika.emlakburadapackageservice.dto.request.PurchasePackageRequest;
 
 import java.math.BigDecimal;
 
 public class PackagePaymentRequestConverter {
 
-    public static com.patika.emlakburadapackageservice.dto.request.PackagePaymentRequest convert(PurchasePackageRequest request, BigDecimal amount) {
-        return com.patika.emlakburadapackageservice.dto.request.PackagePaymentRequest.builder()
+    public static PackagePaymentRequest convert(PurchasePackageRequest request, BigDecimal amount) {
+        return PackagePaymentRequest.builder()
                 .purchasePackageRequest(request)
                 .amount(amount)
                 .build();
